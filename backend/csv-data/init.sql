5d6f131a3308a62160d64a1b5704950455581150
kevingameza <kevin@centrito.co>
2024-04-26 07:19:27 -0500

    change init

--- backend/csv-data/init.sql
+++ backend/csv-data/init.sql
@@ -1,6 +1,6 @@
 TRUNCATE TABLE users, business, reviews, recomendations RESTART IDENTITY CASCADE;
 COPY users (user_id, name, password) FROM '/var/lib/postgresql/csv-data/users.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
 COPY business (business_id, name, address, stars, postal_code, latitude, longitude) FROM '/var/lib/postgresql/csv-data/business_selected.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
-COPY reviews (review_id, user_id, business_id, starts, text, date) FROM '/var/lib/postgresql/csv-data/df_review.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
-COPY recomendations (user_id, business_id, starts) FROM '/var/lib/postgresql/csv-data/predicciones_promedio.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
+COPY reviews (review_id, user_id, business_id, stars, text, date) FROM '/var/lib/postgresql/csv-data/df_review.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
+COPY recomendations (user_id, business_id, stars) FROM '/var/lib/postgresql/csv-data/predicciones_promedio.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
 
