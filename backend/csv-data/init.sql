58a8150b701ea4c6e845f373f1b230a8eb0e1ce9
kevingameza <160034372+kevingameza@users.noreply.github.com>
2024-06-04 15:48:56 -0500

    Update init.sql

--- backend/csv-data/init.sql
+++ backend/csv-data/init.sql
@@ -1,6 +1,6 @@
 TRUNCATE TABLE users, movies, ratings, tags, recommendations RESTART IDENTITY CASCADE;
 COPY users (user_id, password) FROM '/var/lib/postgresql/csv-data/users.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
-COPY movies (movie_id,title,genres,stars,directors,startyear,isadult) FROM '/var/lib/postgresql/csv-data/movies.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
+COPY movies (movie_id, title, genres, stars, directors, startyear, isadult) FROM '/var/lib/postgresql/csv-data/movies.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
 COPY ratings (rating_id, user_id, movie_id, rating) FROM '/var/lib/postgresql/csv-data/ratings.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
 COPY tags (tag_id, user_id, movie_id, tag) FROM '/var/lib/postgresql/csv-data/tags.csv' DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
 
